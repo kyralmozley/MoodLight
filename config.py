55f47f1a3a968f03becf69a0db22309d447ac528
Kyra <kyralmozley@outlook.com>
2019-08-30 16:57:22 +0100

    Create config.py
    add config without API key

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generated via Twitter's Application Management:
+#	https://developer.twitter.com/en/apps
+#--------------------------------------------------------------------------------
+
+consumer_key = ""
+consumer_secret = ""
+access_token_key = ""
+access_token_secret = ""
